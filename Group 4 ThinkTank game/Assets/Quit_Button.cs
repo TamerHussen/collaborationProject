77f397adafe687ac9ae93d037bdfaf0463e299e6
harrylloyd4 <harrylloyd04@icloud.com>
2025-03-05 11:35:52 +0000

    UI !!!!!

---
+++ Group 4 ThinkTank game/Assets/Quit_Button.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Quit_Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitGame()
+    {
+        EditorApplication.isPlaying = false; // Editor version // Testing
+        /*Application.Quit();*/ // Build version
+    }
+
+}
