961a1adfb76aa7d126c5cacf07f3fda4fc83f3ca
harrylloyd4 <harrylloyd04@icloud.com>
2025-04-27 22:33:19 +0100

    Fixed build settings

--- Group 4 ThinkTank game/Assets/Scripts/Quit_Button.cs
+++ Group 4 ThinkTank game/Assets/Scripts/Quit_Button.cs
@@ -8,8 +8,8 @@ public class Quit_Button : MonoBehaviour
     // Start is called before the first frame update
     public void QuitGame()
     {
-        EditorApplication.isPlaying = false; // Editor version // Testing
-        /*Application.Quit();*/ // Build version
+        /*EditorApplication.isPlaying = false;*/ // Editor version // Testing
+        Application.Quit(); // Build version
     }
 
 }
